69366184faf600faa900408ce788099f7b219500
Jacob Smith <smithj09@moravian.edu>
2024-01-21 22:10:34 -0500

    Added Docker file that uses the gunicorn process to launch our Flask server

--- kickoff_app.py
+++ kickoff_app.py
@@ -11,6 +11,9 @@ def create_app():
         return render_template('index.html')
     return app
 
+def launch():
+    return create_app()
+
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)
+    app.run(debug=True, port='8000', host='0.0.0.0')
