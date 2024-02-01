c8708a65970973cbddfb4589348f47cb69676403
Vito Leone <leonev02@moravian.edu>
2024-01-31 23:52:26 -0500

    Add ReactJS app and NodeJS server. Update .gitignore accordingly
    Install project dependencies via "npm install" in project root.
    Run "npm start" to run server and start app in one command.

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'my-react-app/build')));
+
+// Define additional API routes or server logic as needed
+
+// Handle React routing, return all requests to the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'my-react-app/build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
