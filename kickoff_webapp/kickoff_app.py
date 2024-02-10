73dc917e353abcdeb75c31dacdf524672093a786
Nathan <garayn@moravian.edu>
2024-02-10 11:26:50 -0500

    Full link changed to a hyperlink
    A copy button is added too
    Vito will review my pull request when he has time

---
+++ kickoff_webapp/kickoff_app.py
@@ -0,0 +1,25 @@
+# create barebones flask app
+# run with: python kickoff_app.py
+
+from flask import Flask, render_template, jsonify
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @app.route('/data')
+    def get_data():
+        data = {"message": "hello world", "status": 200}
+        return jsonify(data)
+    
+    return app
+
+def launch():
+    return create_app()
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True, port='8000', host='0.0.0.0')
