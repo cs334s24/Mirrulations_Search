0da00999b8b466f48c97dc2242f2ea84da3dbc77
Carl R <reyesc02@moravian.edu>
2024-02-12 13:33:51 -0500

    Update project for creating mongodb docker container

--- down.sh
+++ down.sh
@@ -7,4 +7,7 @@ sudo docker rm $(sudo docker ps -a -q --filter ancestor=kickoff_app)
 # Remove the Docker image
 sudo docker rmi kickoff_app
 
+# Stop and remove the running MongoDB container
+sudo docker stop mongodb && docker rm mongodb
+
 echo "Down script executed successfully."
