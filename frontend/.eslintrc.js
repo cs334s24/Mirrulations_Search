6157b7e58371d47f06bba2161c73ce164b77bc38
Anna Huang <huanga@moravian.edu>
2024-02-16 16:48:58 -0500

    Setup the .eslintrc file

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,14 @@
+export default {
+        extends: [
+            'eslint:recommended',
+            'plugin:react/recommended',
+            'plugin:prettier/recommended', 
+        ],
+        plugins: ['react', 'prettier'], 
+        rules: {
+            // Add any additional rules here
+        },
+    };
+
+
+  
