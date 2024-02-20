aae28c9137ef583d2be573d36f7fe3907c87bddf
Nathan <garayn@moravian.edu>
2024-02-20 12:55:47 -0500

    Redundency removed in the data display
    The ID was removed because the hyperlink is the ID

--- frontend/src/components/SearchResult.js
+++ frontend/src/components/SearchResult.js
@@ -5,7 +5,8 @@ const SearchResult = ({ title, id, link, number_of_comments, number_of_documents
   return (
     <div className="search-result">
       <h2>Title: {title}</h2>
-      <p>Link (ID): <a href={link}>{id}</a></p>
+      <p>ID: {id}</p>
+      <p>Link: <a href={link}>{id}</a></p>
       <p>Number of Comments: {number_of_comments}</p>
       <p>Number of Documents: {number_of_documents}</p>
     </div>
