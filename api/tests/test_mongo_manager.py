6bbaf4485331389851e2a0284cb00462a175f481
Jacob Smith <smithj09@moravian.edu>
2024-02-21 22:34:19 -0500

    Test database manager class
    Implemented tests for the mongo_manager python module
    
    Refactored mongo_manager code to correctly reflect the
    expected behavior and pass the created tests

---
+++ api/tests/test_mongo_manager.py
@@ -0,0 +1,71 @@
+"""Tests for the mongo_manager module"""
+
+import pytest
+import mongomock
+from mirrsearch.api.mongo_manager import MongoManager, ConnectionException
+
+def test_manager_returns_mongo_client_instance():
+    """
+    Tests that the MongoManager class returns an instance that
+    is not None when it is first initialized
+    """
+    client = MongoManager()
+    assert client is not None
+    client.close_instance()
+
+def test_manager_returns_current_instance():
+    """
+    Tests that the get_instance function returns the current
+    database connection
+    """
+    client = MongoManager()
+    assert client.get_instance() is not None
+    client.close_instance()
+
+def test_manager_closes_connection():
+    """
+    Tests that the close_instance function closes the database
+    connection
+    """
+    client = MongoManager()
+    client.close_instance()
+    assert client.get_instance() is None
+
+def test_multiple_connections_is_prohibited():
+    """
+    Tests that there can never be more than one database
+    connection open at a time. A ConnectionException is
+    thrown if multiple connections try to be created
+    """
+    client = MongoManager()
+    try:
+        MongoManager()
+        pytest.fail()
+    except ConnectionException:
+        client.close_instance()
+
+def test_mock_mongo_connection_is_returned():
+    """
+    Tests that if the caller sets the mock paramater
+    to true in the MongoManager initializer, a mock
+    database is returned instead of a real connection
+    """
+    client = MongoManager(mock=True)
+    assert isinstance(client.get_instance(), mongomock.connection.MongoClient)
+    client.close_instance()
+
+def test_default_connection_exception_message():
+    """
+    Tests that the ConnectionException default message
+    is correct
+    """
+    exception = ConnectionException()
+    assert exception.message == 'Error: the connection to the database could not be established'
+
+def test_custom_connection_exception_message():
+    """
+    Tests that a custom message can be passed to the
+    initializer for the ConnectionException class
+    """
+    exception = ConnectionException(message='Test exception message')
+    assert exception.message == 'Test exception message'
