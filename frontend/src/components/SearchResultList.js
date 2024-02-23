1824de075fbb8c7116d88215fea02915f8f46a96
Anna Huang <huanga@moravian.edu>
2024-02-23 14:38:55 -0500

    update changes to the files

--- frontend/src/components/SearchResultList.js
+++ frontend/src/components/SearchResultList.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import SearchResult from './SearchResult'; // Assuming SearchResult.js is in the same directory
+import React from "react";
+import SearchResult from "./SearchResult"; // Assuming SearchResult.js is in the same directory
 
 const SearchResultsList = ({dockets}) => {
-  // Sample data for demonstration
-  const staticDockets = [
-    {
-      title: "Sample Title 1",
-      id: 1,
-      link: "https://example.com/sample1",
-      number_of_comments: 10,
-      number_of_documents: 5
-    },
-    {
-      title: "Sample Title 2",
-      id: 2,
-      link: "https://example.com/sample2",
-      number_of_comments: 5,
-      number_of_documents: 3
-    }
-  ];
+ // Sample data for demonstration
+ const staticDockets = [
+  {
+   title: "Sample Title 1",
+   id: 1,
+   link: "https://example.com/sample1",
+   number_of_comments: 10,
+   number_of_documents: 5,
+  },
+  {
+   title: "Sample Title 2",
+   id: 2,
+   link: "https://example.com/sample2",
+   number_of_comments: 5,
+   number_of_documents: 3,
+  },
+ ];
 
-  return (
-    <div className="search-results-list">
-      {dockets.map((docket, index) => (
-        <SearchResult
-          key={index}
-          title={docket.title}
-          id={docket.id}
-          link={docket.link}
-          number_of_comments={docket.number_of_comments}
-          number_of_documents={docket.number_of_documents}
-        />
-      ))}
-    </div>
-  );
+ return (
+  <div className="search-results-list">
+   {dockets.map((docket, index) => (
+    <SearchResult
+     key={index}
+     title={docket.title}
+     id={docket.id}
+     link={docket.link}
+     number_of_comments={docket.number_of_comments}
+     number_of_documents={docket.number_of_documents}
+    />
+   ))}
+  </div>
+ );
 };
 
 export default SearchResultsList;
--- frontend/src/static/script.js
+++ frontend/src/static/script.js
@@ -1,33 +1,31 @@
 const URL = window.location.href;
 
 function callEndpoint() {
-    // Make a GET request to the /data endpoint
-    fetch('/data')
-        .then(response => response.json())
-        .then(data => {
-            // Display the message in the responseContainer
-            document.getElementById('responseContainer').innerHTML = `<p>${data.message}</p>`;
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-            // Handle errors if needed
-        });
+ // Make a GET request to the /data endpoint
+ fetch("/data")
+  .then((response) => response.json())
+  .then((data) => {
+   // Display the message in the responseContainer
+   document.getElementById("responseContainer").innerHTML = `<p>${data.message}</p>`;
+  })
+  .catch((error) => {
+   console.error("Error fetching data:", error);
+   // Handle errors if needed
+  });
 }
-
 
 // api.js
 const getDummyData = async (searchTerm) => {
-    try {
-      const search = URL + '/api/search_dockets?term=' + searchTerm;
-      const response = await fetch(search)
-      
-      const data = await response.json();
-      console.log(data)
-      return data;
-    } catch (error) {
-      console.error('Error calling the endpoint:', error);
-    }
-  };
-  
-  export default getDummyData;
-  
+ try {
+  const search = URL + "/api/search_dockets?term=" + searchTerm;
+  const response = await fetch(search);
+
+  const data = await response.json();
+  console.log(data);
+  return data;
+ } catch (error) {
+  console.error("Error calling the endpoint:", error);
+ }
+};
+
+export default getDummyData;
