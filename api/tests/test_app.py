d3e052dce856e726a78ae262a68a309569ea73a0
Marie Stella <addom@moravian.edu>
2024-02-23 13:24:27 -0500

    back to green?

--- api/tests/test_app.py
+++ api/tests/test_app.py
@@ -178,4 +178,3 @@ def test_search_comments_endpoint_returns_status_code_400_missing_docket_id(clie
     response = client.get(f'/search_comments?term={search_term}')
     assert response.status_code == 400
     
-    
