cae045ffb095e0a6444896b62562f7621888e354
Kyle Adams <adamsk04@moravian.edu>
2024-03-06 15:16:27 -0500

    Lint javascript code

--- frontend/src/static/script.js
+++ frontend/src/static/script.js
@@ -1,19 +1,5 @@
 const URL = window.location.href;
 
-function callEndpoint() {
- // Make a GET request to the /data endpoint
- fetch("/data")
-  .then((response) => response.json())
-  .then((data) => {
-   // Display the message in the responseContainer
-   document.getElementById("responseContainer").innerHTML = `<p>${data.message}</p>`;
-  })
-  .catch((error) => {
-   console.error("Error fetching data:", error);
-   // Handle errors if needed
-  });
-}
-
 // api.js
 const getDummyData = async (searchTerm) => {
  try {
