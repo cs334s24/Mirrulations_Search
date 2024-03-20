473ccb6c74da5d1a6a0e3aa7c9dcaaf8b0df9f06
Dylan Fodor <df4182@gmail.com>
2024-03-20 14:46:37 -0400

    Added functionality for inserting .htm files

--- api/src/mirrsearch/db/mongo_db.py
+++ api/src/mirrsearch/db/mongo_db.py
@@ -6,12 +6,15 @@
 import os
 import json
 from pymongo import MongoClient
+import html
+
 
 # Connection URI
 URI = 'mongodb://localhost:27017'
 
 # Database Name
 DB_NAME = 'mongoSample'
+
 
 def connect_to_mongodb():
     """ Function to connect to MongoDB and ensure collections exist """
@@ -34,23 +37,6 @@ def insert_txt_data(collection, data):
     collection.insert_one({'data': data})
 
 
-def add_data_to_database(root_folder):
-    """ Function to add data to the database """
-    for root, _, files in os.walk(root_folder):
-        if root.split('/')[-1] == 'docket':
-            for file in files:
-                if file.endswith('.json') or file.endswith('.txt'):
-                    insert_docket_file(os.path.join(root, file), database['docket'])
-        if root.split('/')[-1] == 'comments':
-            for file in files:
-                if file.endswith('.json') or file.endswith('.txt'):
-                    insert_docket_file(os.path.join(root, file), database['comments'])
-        if root.split('/')[-1] == 'documents':
-            for file in files:
-                if file.endswith('.json') or file.endswith('.txt'):
-                    insert_docket_file(os.path.join(root, file), database['documents'])
-
-
 def insert_docket_file(file, collection):
     """ Function to insert a JSON or HTM file into a collection """
     if file.endswith('.json'):
@@ -61,6 +47,27 @@ def insert_docket_file(file, collection):
         with open(file, 'r', encoding='utf-8') as f:
             data = f.read()
             insert_txt_data(collection, data)
+    if file.endswith('.htm'):
+        with open(file, 'r', encoding='utf-8', errors="ignore") as f:
+            data = html.unescape(f.read())
+            insert_txt_data(collection, data)
+
+
+def add_data_to_database(root_folder):
+    """ Function to add data to the database """
+    for root, _, files in os.walk(root_folder):
+        if root.split('/')[-1] == 'docket':
+            for file in files:
+                if file.endswith('.json') or file.endswith('.txt') or file.endswith('.htm'):
+                    insert_docket_file(os.path.join(root, file), database['docket'])
+        if root.split('/')[-1] == 'comments':
+            for file in files:
+                if file.endswith('.json') or file.endswith('.txt') or file.endswith('.htm'):
+                    insert_docket_file(os.path.join(root, file), database['comments'])
+        if root.split('/')[-1] == 'documents':
+            for file in files:
+                if file.endswith('.json') or file.endswith('.txt') or file.endswith('.htm'):
+                    insert_docket_file(os.path.join(root, file), database['documents'])
 
 
 def clear_db(database):
