50b45b38abe33977c7b4a35d7915ccc1dccfb67b
Vito Leone <leonev02@moravian.edu>
2024-03-22 15:21:59 -0400

    Made a dummy endpoint and reformatted docket info

--- frontend/src/components/Docket.js
+++ frontend/src/components/Docket.js
@@ -1,18 +1,19 @@
 import React from "react";
 import "../styles/Docket.css"; // Import the CSS file
 
-const Docket = ({title, id, link,docket_type, documents_containing,total_documents,date_range,comments_containing,total_comments}) => {
+const Docket = ({title, id, link,docket_type, documents_containing,total_documents,date_range, comment_date_range,comments_containing,total_comments}) => {
  return (
   <div className="search-result">
-   <h2>Title: {title}</h2>
+   <h2>{title}</h2>
+   <p>{docket_type}</p>
    <p>
-    Link (ID): <a href={link}>{id}</a>
+    <a href={link}>{id}</a>
    </p>
-   <p>Number of Comments: {comments_containing}/{total_comments}</p>
-   <p>Number of Documents: {documents_containing}/{total_documents}</p>
-   <p>Date Range: {date_range}</p>
-    <p>Comment Date Range: {comment_date_range}</p>
-    <p>Docket Type:{docket_type}</p>
+   <p>{date_range}</p>
+   <p>Related of Comments: {comments_containing}/{total_comments}</p>
+    <p2>Comment Date Range: {comment_date_range}</p2>
+   <p>Related of Documents: {documents_containing}/{total_documents}</p>
+  
   </div>
  );
 };
