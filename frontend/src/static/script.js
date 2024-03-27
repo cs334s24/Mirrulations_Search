4ac364e3304d5ec8b3c286049287190cd486d380
Vito Leone <leonev02@moravian.edu>
2024-03-27 09:00:08 -0400

    First draft of docket

--- frontend/src/static/script.js
+++ frontend/src/static/script.js
@@ -15,24 +15,26 @@ export const getDummyData = async (searchTerm) => {
 };
 
 export const getDummyDataDemo = async () => {
-    try{
-        const data = [{
-            title: "Tribal Self Gevernance", 
-            id: "IHS-2008-1", 
-            //Add Agency Name
-            link: "http://localhost",
-            docket_type: "Notice", 
-            documents_containing: 54,
-            total_documents: 54,
-            date_range: "2008/3/13 - 2010/4/25",
-            comment_date_range: "2008/3/13 - 2010/4/25",
-            comments_containing:20,
-            total_comments: 100
-        }]
-        return data;
-    } catch (error) {
-     console.error("Error calling the endpoint:", error);
-    }
-}
+ try {
+  const data = [
+   {
+    title: "Proposed Informaation Collestion: Final Rule to Implememnt Title V of the Tribal...",
+    id: "IHS-2005-0001",
+    //Add Agency Name
+    link: "https://www.regulations.gov/docket/IHS-2005-0001",
+    docket_type: "Notice",
+    documents_containing: 54,
+    total_documents: 54,
+    date_range: "2008/3/13 - 2010/4/25",
+    comment_date_range: "2008/3/13 - 2010/4/25",
+    comments_containing: 20,
+    total_comments: 100,
+   },
+  ];
+  return data;
+ } catch (error) {
+  console.error("Error calling the endpoint:", error);
+ }
+};
 
 // export default [getDummyData, getDummyDataDemo];
