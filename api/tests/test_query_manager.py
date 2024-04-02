f6125a26d4271b188fb317ea98044a1c3bbfa1ab
Jacob Smith <smithj09@moravian.edu>
2024-04-02 17:15:33 -0400

    Address more pylint errors

--- api/tests/test_query_manager.py
+++ api/tests/test_query_manager.py
@@ -1,13 +1,23 @@
+"""
+Module to test the query manager
+"""
+
 from mirrsearch.api.mock_database_manager import MockMongoDatabase
 from mirrsearch.api.query_manager import MongoQueryManager
 
 def test_search_dockets():
+    """
+    Function to test the search_dockets function in the MongoQueryManager class
+    """
     manager = MockMongoDatabase()
     query_manager = MongoQueryManager(manager)
     response = query_manager.search_dockets('test')
     assert response['data']['search_term'] == 'test'
 
 def test_search_comments():
+    """
+    Function to test the search_comments function in the MongoQueryManager class
+    """
     manager = MockMongoDatabase()
     query_manager = MongoQueryManager(manager)
     response = query_manager.search_comments('test', 'test')
