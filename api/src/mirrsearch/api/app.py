6e5f3304664cd4fa37f5262dae30eedae3bebd3e
Nathan <garayn@moravian.edu>
2024-04-05 13:26:57 -0400

    Query search_documents endpoint
    Added code to app.py, database_manager.py, and query_manager.py

--- api/src/mirrsearch/api/app.py
+++ api/src/mirrsearch/api/app.py
@@ -11,109 +11,100 @@ from mirrsearch.api.mock_database_manager import MockMongoDatabase
 from mirrsearch.api.mock_query_manager import MockMongoQueries
 
 def create_app(query_manager):
-    """
-    Create the application instance and define the routes
-    """
-    app = Flask(__name__)
-    CORS(app)
+   """
+   Create the application instance and define the routes
+   """
+   app = Flask(__name__)
+   CORS(app)
 
-    @app.route('/data')
-    def get_data():
-        data = {"message": "hello world", "status": 200}
-        return jsonify(data)
+   @app.route('/data')
+   def get_data():
+       data = {"message": "hello world", "status": 200}
+       return jsonify(data)
 
-    @app.route('/search_dockets')
-    def search_dockets():
-        # Obtains the search term
-        search_term = request.args.get('term')
+   @app.route('/search_dockets')
+   def search_dockets():
+       # Obtains the search term
+       search_term = request.args.get('term')
 
-        # If a search term is not provided, the server will return this JSON and a 400 status code
-        if not search_term:
-            response = {}
-            response['error'] = {'code': 400,
-                                 'message': 'Error: You must provide a term to be searched'}
-            return jsonify(response), 400
-        
-        response = query_manager.search_dockets(search_term)
+       # If a search term is not provided, the server will return this JSON and a 400 status code
+       if not search_term:
+           response = {}
+           response['error'] = {'code': 400,
+                                'message': 'Error: You must provide a term to be searched'}
+           return jsonify(response), 400
+      
+       response = query_manager.search_dockets(search_term)
 
-        return jsonify(response)
+       return jsonify(response)
 
-    @app.route('/search_documents')
-    def search_documents():
-        response = {}
+   @app.route('/search_documents')
+   def search_documents():
 
-        # Obtains the search term and document id from a prior request
-        search_term = request.args.get('term')
-        document_id = request.args.get('document_id')
+       # Obtains the search term and document id from a prior request
+       search_term = request.args.get('term')
+       docket_id = request.args.get('docket_id')
 
-        # If a search term is not provided, the server will return this JSON and a 400 status code
-        if not search_term:
-            response['error'] = {'code': 400,
-                                 'message': 'Error: You must provide a term to be searched'}
-            return jsonify(response), 400
+       # If a search term is not provided, the server will return this JSON and a 400 status code
+       if not search_term:
+           response = {}
+           response['error'] = {'code': 400,
+                                'message': 'Error: You must provide a term to be searched'}
+           return jsonify(response), 400
 
-        # If the search term is valid, data will be ingested into the JSON response
-        response['data'] = {
-            'search_term': search_term,
-            'comments': []
-        }
-        response['data']['comments'].append({
-            "author": "Environmental Protection Agency",
-            "date_posted": "Dec 22, 2003",
-            "link": "https://www.regulations.gov/document/EPA-HQ-OAR-2003-0083-0794",
-            "document_id": document_id
-           })
-        return jsonify(response)
+       response = query_manager.search_documents(search_term, docket_id)
+      
+       return jsonify(response)
 
-    @app.route('/search_comments')
-    def search_comments():
-        response = {}
+   @app.route('/search_comments')
+   def search_comments():
+       response = {}
 
-        # Obtains the search term and docket id from a prior request
-        search_term = request.args.get('term')
-        docket_id = request.args.get('docket_id')
+       # Obtains the search term and docket id from a prior request
+       search_term = request.args.get('term')
+       docket_id = request.args.get('docket_id')
 
-        # If a search term is not provided, the server will return this JSON and a 400 status code
-        if not search_term:
-            response['error'] = {'code': 400,
-                                 'message': 'Error: You must provide a term to be searched'}
-            return jsonify(response), 400
-        if not docket_id:
-            response['error'] = {'code': 400,
-                                 'message': 'Error: You must provide a docket_id to be searched'}
-            return jsonify(response), 400
+       # If a search term is not provided, the server will return this JSON and a 400 status code
+       if not search_term:
+           response['error'] = {'code': 400,
+                                'message': 'Error: You must provide a term to be searched'}
+           return jsonify(response), 400
+       if not docket_id:
+           response['error'] = {'code': 400,
+                                'message': 'Error: You must provide a docket_id to be searched'}
+           return jsonify(response), 400
 
-        # If the search term is valid, data will be ingested into the JSON response
-        response['data'] = {
-            'search_term': search_term,
-            'comments': []
-        }
+       # If the search term is valid, data will be ingested into the JSON response
+       response['data'] = {
+           'search_term': search_term,
+           'comments': []
+       }
 
-        response['data']['comments'].append({
-            "author": "Department of Health and Human Services",
-            "date_posted": "Apr 14, 2011",
-            "link": "https://www.regulations.gov/comment/HHS-OS-2010-0014-0032",
-            "docket_id": docket_id
-           })
-        return jsonify(response)
+       response['data']['comments'].append({
+           "author": "Department of Health and Human Services",
+           "date_posted": "Apr 14, 2011",
+           "link": "https://www.regulations.gov/comment/HHS-OS-2010-0014-0032",
+           "docket_id": docket_id
+          })
+       return jsonify(response)
 
-    return app
+   return app
 
 def launch(database):
-    """
-    Launch the Flask app
-    """
-    if database == 'mongo':
-        database_manager = MongoManager()
-        query_manager = MongoQueryManager(database_manager)
-        return create_app(query_manager)
-    elif database == 'mockMongo':
-        database_manager = MockMongoDatabase()
-        query_manager = MockMongoQueries(database_manager)
-        return create_app(query_manager)
-    
-    # TODO: Handle case where they do not provide which database they want to use
+   """
+   Launch the Flask app
+   """
+   if database == 'mongo':
+       database_manager = MongoManager()
+       query_manager = MongoQueryManager(database_manager)
+       return create_app(query_manager)
+   elif database == 'mockMongo':
+       database_manager = MockMongoDatabase()
+       query_manager = MockMongoQueries(database_manager)
+       return create_app(query_manager)
+  
+   # TODO: Handle case where they do not provide which database they want to use
 
 if __name__ == '__main__':
-    flask_app = create_app()
-    flask_app.run(debug=True, port=8000, host='0.0.0.0')
+   flask_app = create_app()
+   flask_app.run(debug=True, port=8000, host='0.0.0.0')
--- api/src/mirrsearch/api/query_manager.py
+++ api/src/mirrsearch/api/query_manager.py
@@ -2,46 +2,99 @@ from mirrsearch.api.database_manager import DatabaseManager
 
 class QueryManager:
 
-    __manager = None
+   __manager = None
 
-    def __init__(self, database_manager):
-        self.__manager = database_manager
-
+   def __init__(self, database_manager):
+       self.__manager = database_manager
 
 class MongoQueryManager(QueryManager):
 
-    __manager = None
+   __manager = None
 
-    def search_dockets(self, search_term):
-        response = {}
+   def search_dockets(self, search_term):
+       response = {}
 
-        # Uses the database manager to query the dockets
-        search = self.__manager.search_dockets(search_term)
-            
-        # If the search term is valid, data will be ingested into the JSON response
-        response['data'] = {
-            'search_term': search_term,
-                'dockets': []
-            }
+       # Uses the database manager to query the dockets
+       search = self.__manager.search_dockets(search_term)
+          
+       # If the search term is valid, data will be ingested into the JSON response
+       response['data'] = {
+           'search_term': search_term,
+               'dockets': []
+           }
 
-        for doc in search:
-            title = doc['attributes']['title']
-            id = doc['id']
-            link = doc['links']['self']
-            # TODO: Query comments and documents collections to get count of each using docket ID
-            number_of_comments = 0
-            number_of_documents = 0
-            response['data']['dockets'].append({
-                'title': title,
-                'id': id,
-                'link': link,
-                'number_of_comments': number_of_comments,
-                'number_of_documents': number_of_documents
-            })
-                
-        # self.__manager.close_instance()
+       for doc in search:
+           title = doc['attributes']['title']
+           id = doc['id']
+           link = doc['links']['self']
+           # TODO: Query comments and documents collections to get count of each using docket ID
+           number_of_comments = 0
+           number_of_documents = 0
+           response['data']['dockets'].append({
+               'title': title,
+               'id': id,
+               'link': link,
+               'number_of_comments': number_of_comments,
+               'number_of_documents': number_of_documents
+           })
 
-        return response
-    
-    def __init__(self, database_manager: DatabaseManager):
-        self.__manager = database_manager
+       return response
+  
+   def search_documents(self, search_term, docket_id):
+       response = {}
+
+       # Uses the database manager to query the documents
+       search = self.__manager.search_documents(search_term, docket_id)
+
+       # If the search term is valid, data will be ingested into the JSON response
+       response['data'] = {
+           'search_term': search_term,
+           'docket_id': docket_id,
+           'documents': []
+       }
+
+       for document in search:
+           author = document['attributes']['authors']
+           date_posted = document['postedDate']
+           link = document['links']['self']
+           docket_id = document['docketId']
+           response['data']['documents'].append({
+               'author': author,
+               'date_posted': date_posted,
+               'link': link,
+               'docket_id': docket_id,
+               })
+
+       return response
+  
+   def search_comments(self, search_term, docket_id):
+       response = {}
+
+       # Uses the database manager to query the comments
+       search = self.__manager.search_comments(search_term, docket_id)
+
+
+       # If the search term is valid, data will be ingested into the JSON response
+       response['data'] = {
+           'search_term': search_term,
+           'docket_id': docket_id,
+           'comments': []
+       }
+
+       for comment in search:
+           author = comment['attributes']['lastName']
+           date_posted = comment['postedDate']
+           link = comment['links']['self']
+           docket_id = comment['docketId']
+           response['data']['comments'].append({
+               'author': author,
+               'date_posted': date_posted,
+               'link': link,
+               'docket_id': docket_id,
+               })
+
+       return response
+  
+   def __init__(self, database_manager: DatabaseManager):
+       self.__manager = database_manager
+       
