16a1c4bea5121f5d833383456ca296c1b743b50f
Marie Stella <addom@moravian.edu>
2024-04-14 15:54:53 -0400

    homepage has a background picture now. however not ready to merge need to make additinal changes

--- frontend/src/components/Homepage_image.js
+++ frontend/src/components/Homepage_image.js
@@ -4,18 +4,16 @@ import SearchBar from "./SearchBar";
 import "../styles/SearchBar.css";
 import Button from "./Button";
 import "../styles/Button.css";
-import images from "/Users/annahuang/Desktop/Capstone/Mirrulations_1Search/frontend/src/images/homepage pic.jpeg";
-
+import images from "./images/homepage pic.jpeg";
 
 const Homepage_image = () => {
-  return (
-    <div className="Homepage_image">
-    <img src={images} alt="this will be the homepage background picture" />
-    <div className="search-bar">
-      <SearchBar />
-      <Button />
-    </div>
-    </div>
-  );
+ return (
+  <div className="Homepage_image">
+   <div className="search-bar">
+    <SearchBar />
+    <Button />
+   </div>
+  </div>
+ );
 };
 export default Homepage_image;
