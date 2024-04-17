f66c1c0cf463524203e3f0915c018d537b7f7dd6
Vito Leone <leonev02@moravian.edu>
2024-04-17 12:09:01 -0400

    Add parameters to https request

--- frontend/src/static/script.js
+++ frontend/src/static/script.js
@@ -17,7 +17,7 @@ export const fetchDockets = async (searchTerm) => {
 
 export const zipFiles = async (email, docketID) => {
  try {
-  const search = URL + `api/zip_data`;
+  const search = URL + `api/zip_data?to_email=${email}&docket_id=${docketID}`;
   const response = await fetch(search);
   const data = await response.json();
   return data;
