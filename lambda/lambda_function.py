3faf7f887e4fcac4e01f9f20f7b515cf23dbf1f9
Shawn Ryer <99298681+ShawnRye@users.noreply.github.com>
2024-04-17 12:56:07 -0400

    Update lambda_function.py to send emails
    This is NOT ready for implementation at this time until Vito's PR can call this correctly. Awaiting further testing

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,12 +1,39 @@
+import smtplib
+import boto3
 import json
-import os 
-
-print('Good morning Capstone!')
 
 def lambda_handler(event, context):
-    #print("Received event: " + json.dumps(event, indent=2))
-    print("value1 = " + event['key1'])
-    print("value2 = " + event['key2'])
-    print("value3 = " + event['key3'])
-    return event['key1']  # Echo back the first key value
-    #raise Exception('Something went wrong')
+    try:
+        # Extract input data from the event
+        to_email = event['to_email']
+        docket_id = event['docket_id']
+
+        client = boto3.client('secretsmanager', region_name='us-east-1')
+        secret_name = "TestingZipSystemLambda"
+        response = client.get_secret_value(SecretId=secret_name)
+        secret_string = response['SecretString']
+        secret_data = json.loads(secret_string)
+        
+        # Extract sender email and password from SecretString
+        sender_email = list(secret_data.keys())[0]
+        sender_password = list(secret_data.values())[0]
+        
+        # Content
+        subject = "Mirrulations Download Started!"
+        message = f"Your download for docket {docket_id} has begun and will be done shortly. Hang tight!"
+        
+        # Connect to the Gmail SMTP server
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            server.starttls()
+            server.login(sender_email, sender_password)
+            
+            # Compose the email
+            email = f'Subject: {subject}\n\n{message}'
+            
+            # Send the email
+            server.sendmail(sender_email, to_email, email)
+            
+        return "Email sent successfully!"
+    except Exception as e:
+        print(f"An error occurred while sending email: {e}")
+        return f"Failed to send email: {e}"
