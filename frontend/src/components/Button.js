c3a90c5e6ccfcc90f2792d9b2b4398503effbdb0
Luke <suppal@moravian.edu>
2024-04-19 14:30:46 -0400

    Updated zip button so only one alert pops up instead of two

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -6,10 +6,9 @@ const Button = () => {
  const [clicked, setClicked] = useState(false);
 
  const handleClick = () => {
-  alert(`An email containing a downloadable version of docket has been sent`);
+  alert(`An email containing a downloadable version of the docket has been sent`);
   setClicked(true);
   zipFiles();
-  alert("The docket is being sent to your email", "Mirrulations");
  };
 
  return (
